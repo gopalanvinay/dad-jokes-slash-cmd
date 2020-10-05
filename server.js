8846b87deec7a0bb272730957b1410ec28215c20
Glitch (hello-express) <none>
2020-10-05 17:17:39 +0000

    🙅🎙 Checkpoint ./server.js:23314134/104

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ app.post('/', async (req, res) => {
   
 })
 
+app.get("/", (request, response) => {
+  console.log("Ping received!");
+  response.sendStatus(200);
+});
+
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
